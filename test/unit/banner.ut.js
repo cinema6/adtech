e569c177be52400b3577ceea82654b7603bb66a9
howard <howard.engelhart@outlook.com>
2014-10-24 17:04:57 -0400

    adding functionality and unit tests

--- test/unit/banner.ut.js
+++ test/unit/banner.ut.js
@@ -1,5 +1,111 @@
 describe('banner',function(){
-    it('should pass',function(){
-        expect(true).toEqual(true);
+    var flush = true, q, banner, mockSUtils, resolveSpy, rejectSpy;
+    beforeEach(function(){
+        if (flush) { for (var m in require.cache){ delete require.cache[m]; } flush = false; }
+        banner       = require('../../lib/banner');
+        mockSUtils   = require('../../lib/soaputils');
+        q            = require('q');
+        resolveSpy   = jasmine.createSpy('resolve');
+        rejectSpy    = jasmine.createSpy('reject');
+
+        spyOn(mockSUtils,'createSoapSSLClient');
+    });
+
+    describe('createClient',function(){
+        it('should resolve with a client if succeeds',function(done){
+            var client = {}, key = {}, cert = {};
+            mockSUtils.createSoapSSLClient.andReturn(q(client));
+            banner.createClient(key,cert)
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).toHaveBeenCalledWith(client);
+                    expect(rejectSpy).not.toHaveBeenCalled(); 
+
+                    var args = mockSUtils.createSoapSSLClient.calls[0].args;
+                    expect(args[0]).toEqual('./wsdl/WSBannerAdmin_v13');
+                    expect(args[1]).toEqual({
+                        strict : true,
+                        endpoint :'https://ws.us-ec.adtechus.com/WSBannerAdmin_v13/'
+                    });
+                    expect(args[2]).toEqual(key);
+                    expect(args[3]).toEqual(cert);
+                })
+                .done(done);
+        });
+
+        it('should reject with an error if it fails', function(done){
+            var err = {};
+            mockSUtils.createSoapSSLClient.andCallFake(function(){
+                return q.reject(err);
+            });
+            banner.createClient()
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).not.toHaveBeenCalled();
+                    expect(rejectSpy).toHaveBeenCalledWith(err); 
+                })
+                .done(done);
+        });
+
+    })
+
+    describe('createBanner', function(){
+        var mockClient;
+
+        beforeEach(function(){
+            mockClient = {
+                createBanner : jasmine.createSpy('createBanner')
+            };
+
+            mockClient.createBanner.andCallFake(function(opts,cb){
+                process.nextTick(function(){
+                    cb(null,{});
+                });
+            });
+        });
+
+        it('maps parameters to parameter properties',function(done){
+            var bann = {  },
+                bannerInfo = {  };
+
+            banner.createBanner(mockClient,12345,bann,bannerInfo,2)
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).toHaveBeenCalled();
+                    expect(rejectSpy).not.toHaveBeenCalled();
+                    
+                    var args = mockClient.createBanner.calls[0].args;
+                    expect(args[0]).toEqual({
+                        bann : bann,
+                        campaignId : 12345,
+                        bannerInfo : bannerInfo,
+                        bannerTimeRangeNo : 2 
+                    });
+                })
+                .done(done);
+        });
+
+        it('sets a default timeRangeNo to 1',function(done){
+            var bann = {  },
+                bannerInfo = {  };
+
+            banner.createBanner(mockClient,12345,bann,bannerInfo)
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).toHaveBeenCalled();
+                    expect(rejectSpy).not.toHaveBeenCalled();
+                    
+                    var args = mockClient.createBanner.calls[0].args;
+                    expect(args[0]).toEqual({
+                        bann : bann,
+                        campaignId : 12345,
+                        bannerInfo : bannerInfo,
+                        bannerTimeRangeNo : 1 
+                    });
+                })
+                .done(done);
+        });
+
+
     });
 });
--- test/unit/soaputils.ut.js
+++ test/unit/soaputils.ut.js
@@ -1,6 +1,82 @@
 describe('soaputils',function(){
-    it('should pass',function(){
-        expect(true).toEqual(true);
+    var flush = true, q, soapUtils, resolveSpy, rejectSpy;
+    beforeEach(function(){
+        if (flush) { for (var m in require.cache){ delete require.cache[m]; } flush = false; }
+        soapUtils  = require('../../lib/soaputils');
+        q          = require('q');
+        resolveSpy = jasmine.createSpy('resolve');
+        rejectSpy  = jasmine.createSpy('reject');
+    });
+
+    describe('createSoapSSLClient',function(){
+        var mockClient, mockFs, mockSoap;
+        beforeEach(function(){
+            mockFs     = require('fs');
+            mockSoap   = require('soap');
+
+            mockClient = {
+                setSecurity : jasmine.createSpy('setSecurity')
+            };
+
+            spyOn(mockFs,'readFileSync');
+            spyOn(mockSoap,'createClient');
+        });
+
+        it('should resolve with an ssl client if succeeds',function(done){
+            mockFs.readFileSync.andReturn({});
+            mockSoap.createClient.andCallFake(function(wsdl, opts,cb){
+                process.nextTick(function(){
+                    cb(null,mockClient);
+                });
+            });
+            soapUtils.createSoapSSLClient({},{},'keypath','certpath')
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).toHaveBeenCalledWith(mockClient);
+                    expect(rejectSpy).not.toHaveBeenCalled(); 
+
+                    expect(mockClient.setSecurity).toHaveBeenCalled();
+                })
+                .done(done);
+        });
+
+        it('should reject if key and cert not passed',function(done){
+            soapUtils.createSoapSSLClient({},{})
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).not.toHaveBeenCalled();
+                    expect(rejectSpy).toHaveBeenCalledWith(
+                        'SSL Key and Cert parameters are required!'
+                    ); 
+                })
+                .done(done);
+        });
+
+        it('should reject with an error if missing wsdl', function(done){
+            soapUtils.createSoapSSLClient()
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).not.toHaveBeenCalled();
+                    expect(rejectSpy).toHaveBeenCalledWith('Wsdl parameter is required!'); 
+                })
+                .done(done);
+        });
+
+        it('should reject with an error if soap create client fails', function(done){
+            var err = {};
+            mockSoap.createClient.andCallFake(function(wsdl, opts, cb){
+                process.nextTick(function(){
+                    cb(err);
+                });
+            });
+            soapUtils.createSoapSSLClient({},{},'keypath','certpath')
+                .then(resolveSpy,rejectSpy)
+                .then(function(){
+                    expect(resolveSpy).not.toHaveBeenCalled();
+                    expect(rejectSpy).toHaveBeenCalledWith(err); 
+                })
+                .done(done);
+        });
     });
 });
 
